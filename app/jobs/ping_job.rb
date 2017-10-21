1b9b1c5395fb2cb04edfca3f35f3c57f816d1d5c
Josef Diago <josef.diago@gmail.com>
2017-10-21 14:21:54 -0400

    renames test_job

--- app/jobs/ping_job.rb
+++ app/jobs/ping_job.rb
@@ -1,6 +1,6 @@
 require 'twilio-ruby'
 
-class TestJob < ApplicationJob
+class PingJob < ApplicationJob
   def perform
     client = Twilio::REST::Client.new(
       ENV['TWILIO_ACCOUNT_SID'],
@@ -10,7 +10,7 @@ class TestJob < ApplicationJob
     Contact.all.each do |contact|
       client.messages.create(
         messaging_service_sid: ENV['TWILIO_MESSAGING_SERVICE_SID'],
-        to: "+1#{contact.number}",
+        to: contact.number,
         body: "PLS RESPOND"
       )
     end
