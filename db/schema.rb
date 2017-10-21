800b514f84589022d5a69ec3ecdfeca45283f441
Josef Diago <josef.diago@gmail.com>
2017-10-21 12:48:56 -0400

    adds contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021155249) do
+ActiveRecord::Schema.define(version: 20171021164639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "crono_jobs", force: :cascade do |t|
     t.string "job_id", null: false
